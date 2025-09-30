278cae1fa9c8873943c9357847cf68ae897d0204
bururun <bururun@users.noreply.github.com>
2025-09-30 16:41:40 +0300

    chore(utils): improve UI/UX

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,27 @@ function newFunction5() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for JourneyTracker
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 16
+function newFunction16() {
+    return 16;
+}
+
+module.exports = { formatData, validateInput };
